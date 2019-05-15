02abe5571a28d171cc6f686b99a041e011226c16
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-05-15 11:48:57 +0800

    convert head encoding

--- Utilities/cpp2unicode.rkt
+++ Utilities/cpp2unicode.rkt
@@ -9,11 +9,17 @@
       (λ [/dev/utf8out] (displayln cpp /dev/utf8out)))
     (printf "[Converted]~a~n" src.cpp)))
 
-(define cpp-root (get-directory "Choose the root directory of the java sources"))
+(define cpp-root (get-directory "Choose the root directory of the cpp sources"))
 
 (when (path? cpp-root)
   (for ([src.cpp (in-directory cpp-root)] #:when (regexp-match? #px"[.]cpp$" src.cpp))
     (define maybe-ok (with-handlers ([exn? values]) (bytes->string/utf-8 (file->bytes src.cpp))))
     (cond [(not (exn? maybe-ok)) (printf "[Skipped]~a~n" src.cpp)]
           [else (with-handlers ([exn? (λ [e] (fprintf (current-error-port) "[Failed]~a: ~a~n" (exn-message e)))])
-                  (gbk->utf8 src.cpp))])))
+                  (gbk->utf8 src.cpp))]))
+
+  (for ([src.h (in-directory cpp-root)] #:when (regexp-match? #px"[.]h$" src.h))
+    (define maybe-ok (with-handlers ([exn? values]) (bytes->string/utf-8 (file->bytes src.h))))
+    (cond [(not (exn? maybe-ok)) (printf "[Skipped]~a~n" src.h)]
+          [else (with-handlers ([exn? (λ [e] (fprintf (current-error-port) "[Failed]~a: ~a~n" (exn-message e)))])
+                  (gbk->utf8 src.h))])))
